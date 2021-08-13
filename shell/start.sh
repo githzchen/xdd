ab834c25a5b198139b4cb76e214005b74c873680
utterliar1 <211538958@qq.com>
2021-08-13 14:06:52 +0800

    update

--- shell/start.sh
+++ shell/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [ ! -d "/ql" ];then
   cd /jd/xdd && nohup ./xdd_linux_arm64 > xdd.txt 2>&1 &
+  echo "xdd程序正在启动中，请稍后。。。"
 else
   cd /ql/xdd && nohup ./xdd_linux_arm64 > xdd.txt 2>&1 &
+  echo "xdd程序正在启动中，请稍后。。。"
 fi
--- shell/stop.sh
+++ shell/stop.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 ps -ef | grep ./xdd | grep -v grep | awk '{print $1}' | xargs kill -9
+echo "xdd程序已关闭"
